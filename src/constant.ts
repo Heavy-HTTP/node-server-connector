e333a63488040bc29bc158e89107750d58b9c7d5
Pasindu <pasinduath@gmail.com>
2022-11-17 23:17:34 +1100

    init connector

---
+++ src/constant.ts
@@ -0,0 +1,7 @@
+export const X_HEAVY_HTTP_ID = "x-heavy-http-id";
+export const X_HEAVY_HTTP_ACTION = "x-heavy-http-action";
+export const X_HEAVY_HTTP_ACTIONS = {
+    INIT:"init",
+    SEND_ERROR:"send-error",
+    SEND_SUCCESS:"send-success"
+}
