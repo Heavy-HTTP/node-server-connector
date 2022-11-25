6303db0a6db49ec8c6b63fa0a60ae4937c289f27
Pasindu <pasinduath@gmail.com>
2022-11-25 21:08:15 +1100

    implemented download flow

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,6 @@
 export const X_HEAVY_HTTP_ID = "x-heavy-http-id";
 export const X_HEAVY_HTTP_ACTION = "x-heavy-http-action";
+export const X_HEAVY_HTTP_STREAM = "x-heavy-http-stream";
 export const X_HEAVY_HTTP_ACTIONS = {
     INIT:"init",
     SEND_ERROR:"send-error",
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { X_HEAVY_HTTP_ACTION, X_HEAVY_HTTP_ACTIONS, X_HEAVY_HTTP_ID } from "./constant";
+import { X_HEAVY_HTTP_ACTION, X_HEAVY_HTTP_ACTIONS, X_HEAVY_HTTP_ID, X_HEAVY_HTTP_STREAM } from "./constant";
 import { IncomingMessage, ServerResponse } from "http";
 
 interface ConnectorConfig {
@@ -22,7 +22,7 @@ interface Transporter {
 export const connector = (connectorConfig: ConnectorConfig, transporter: Transporter): Function => {
 
     if (connectorConfig.responseSize < 0) {
-        throw new Error("Response Size must be a non-negative integer")
+        throw new Error("Response Size must be a non-negative value")
     }
 
     return async function (req: IncomingMessage, res: ServerResponse, next: (err?: any) => any) {
@@ -90,8 +90,144 @@ export const connector = (connectorConfig: ConnectorConfig, transporter: Transpo
                 }
 
             }
+
+
+            const responseMap = {
+                content: new Map(),
+                isStreamed: false,
+                writeHead:()=>{},
+                write:()=>{}
+            }
+
+            const oldWriteHead = res.writeHead
+
+            res.writeHead = function () {
+
+                responseMap.isStreamed = res.hasHeader('dsdsdsdsdsd');
+
+                let possiableHeaders = null;
+                if (!responseMap.isStreamed) {
+                    if (arguments.length === 3) {
+                        possiableHeaders = arguments[2];
+                    } else if (arguments.length === 2) {
+                        possiableHeaders = arguments[1];
+                    }
+
+                    if (possiableHeaders) {
+
+                        if (Array.isArray(possiableHeaders) && possiableHeaders.includes(X_HEAVY_HTTP_STREAM)) {
+                            responseMap.isStreamed = true;
+
+                        } else if (possiableHeaders[X_HEAVY_HTTP_STREAM]) {
+                            responseMap.isStreamed = true;
+                        }
+                    }
+                }
+
+                if (responseMap.isStreamed) {
+                    const argumentsTyped: any = arguments;
+                    oldWriteHead.apply(res, argumentsTyped);
+                }else {
+                    responseMap.writeHead = ()=>{
+                        const argumentsTyped: any = arguments;
+                        oldWriteHead.apply(res, argumentsTyped);
+                    }
+                }
+                return res;
+            }
+
+            const oldWrite = res.write
+
+            res.write = function (data: any, encodingOrCb: BufferEncoding | undefined | ((error: Error | null | undefined) => void), cb?: (error: Error | null | undefined) => void | undefined): boolean {
+
+                if (responseMap.isStreamed) {
+                    const argumentsTyped: any = arguments;
+                    oldWrite.apply(res, argumentsTyped);
+                } else {
+                    res.getHeaders()
+                    responseMap.content.set('data',data);
+                    responseMap.content.set('callback',()=>{});
+
+                    if(encodingOrCb && typeof encodingOrCb ==='string'){
+                        responseMap.content.set('encoding',encodingOrCb);
+                    }else if(encodingOrCb){
+                        responseMap.content.set('callback',encodingOrCb);
+                    }
+
+                    if(cb){
+                        responseMap.content.set('callback',cb);
+                    }
+
+                    responseMap.write = ()=>{
+                        const argumentsTyped: any = arguments;
+                        oldWrite.apply(res, argumentsTyped);
+                    }
+                }
+                return true;
+            }
+
+            const oldSend = res.end
+
+            res.end = function (endData: any, endDataEncodingOrCb?: BufferEncoding | undefined | (() => void), cb?: () => void | undefined): ServerResponse<IncomingMessage> {
+
+                let finalData = Buffer.from('');
+                
+                const data  = responseMap.content.get('data');
+                const encoding  = responseMap.content.get('encoding');
+                const callBack  = responseMap.content.get('callback');
+
+                let endCallback = ()=>{
+                    if(cb){
+                        cb();
+                    }
+                }
+
+                if (Buffer.isBuffer(data)) {
+                        finalData = Buffer.concat([finalData, data]);
+                } else if (typeof data === 'string') {
+                        if (encoding) {
+                            finalData = Buffer.concat([finalData, Buffer.from(data, encoding)])
+                        } else {
+                            finalData = Buffer.concat([finalData, Buffer.from(data)])
+                        }
+                    }
+
+            
+
+                if (endData) {
+                    if (Buffer.isBuffer(endData)) {
+                        finalData = Buffer.concat([finalData, endData]);
+                    } else if (typeof endData === 'string') {
+                        if(endDataEncodingOrCb){
+                            if (typeof endDataEncodingOrCb === 'string') {
+                                finalData = Buffer.concat([finalData, Buffer.from(endData, endDataEncodingOrCb)])
+                            } else {
+                                endCallback = endDataEncodingOrCb;
+                                finalData = Buffer.concat([finalData, Buffer.from(endData)])
+                            }
+                        }else {
+                            finalData = Buffer.concat([finalData, Buffer.from(endData)])
+                        }
+                    
+                    }
+                }
+
+                if (finalData.byteLength > connectorConfig.responseSize) {
+                    callBack();
+                    res.setHeader(X_HEAVY_HTTP_ACTION,X_HEAVY_HTTP_ACTIONS.SEND_ERROR);
+                    res.setHeader(X_HEAVY_HTTP_ID,'sdsdsdsds');
+                    responseMap.writeHead();
+                    return oldSend.apply(res, ["",'utf-8',endCallback])
+                } 
+                    
+                responseMap.writeHead();
+                responseMap.write();
+                const argumentsTyped: any = arguments;
+                return oldSend.apply(res, argumentsTyped);
+            }
+
         } catch (e) {
-            throw new Error("Heavy Request Failed, caused by: "+(e as Error).stack+"\n")
+            throw new Error("Heavy Request Failed, caused by: " + (e as Error).stack + "\n")
         }
         next();
     };
